7c942c9fc380f4488504c88713947e0897b2c7e6
Felip3 <juanfeliperamirezcardona0@gmail.com>
2025-11-18 19:46:57 -0500

    update

--- api.py
+++ api.py
@@ -8,8 +8,9 @@ from fastapi import FastAPI  # Crea la API web
 from fastapi.middleware.cors import CORSMiddleware  # Habilita CORS para frontends
 from contextlib import asynccontextmanager
 
-#conexion a una base de datos
-NOMBRE_BASEDATOS="ranking.bd"
+
+#conexion hacia una bd
+NOMBRE_BASEDATOS="ranking.db"
 FUENTE_DATOS="fifa_ranking.json"
 
 #modelo de datos (tabla)
@@ -22,48 +23,51 @@ class Ranking(BaseModel):
 class CuerpoRanking(BaseModel):
     items:List[Ranking]
 
-#operaciones o servicios que voy a exponer en el front
 
-#Abrir conexion a bd
+#operaciones o servicios que voy a entregarle al front
+ 
+#abrir conexion a bd
+
 def crear_conexion():
     return sqlite3.connect(NOMBRE_BASEDATOS)
-#Crear tabla que almacena el ranking si no existe
+
+#crear la tabla que almcena el ranking si no existe
 def crear_tabla():
-    conexion=crear_conexion()
-    cursor=conexion.cursor()
+    conexion=crear_conexion() 
+    cursor=conexion.cursor() #ubicate en la bd despues de conectarte
     cursor.execute("""
-                    CREATE TABLA IF NOT EXISTS ranking_fifa(
-                        rango INTEGER PRIMARY KEY,
-                        pais TEXT NOT NULL,
-                        puntos TEXT NOT NULL
-                    )
-                    """)
-    conexion.commit()
-    conexion.close()
-    
-#Modificar el ranking
-def modificar_ranking(lista:List[Ranking]):
-    conexion=crear_conexion()
-    cursor=conexion.cursor()
-    cursor.execute("DELETE FROM ranking_fifa")
-    cursor.executemany(
-        "INSERT INTO ranking_fifa (rango, pais, puntos) VALUES (?, ?, ?)",
-        [(elementoDeLaLista.rango,elementoDeLaLista.pais,elementoDeLaLista.puntos)for elementoDeLaLista in lista]
-    )
-    # for item in lista:
-    #     cursor.execute("INSERT INTO ranking_fifa (rango, pais, puntos) VALUES (?, ?, ?)",
-    #                     (item.rango, item.pais, item.puntos))
-    conexion.commit()
+
+            CREATE TABLE IF NOT EXISTS ranking_fifa(
+                   rango INTEGER PRIMARY KEY,
+                   pais TEXT NOT NULL,
+                   puntos TEXT NOT NULL
+            )
+
+    """)
+    conexion.commit() #Ejecuta la consulta en la bd
     conexion.close()
 
-#Leer el ranking
-def leer_ranking()->List[Ranking]:
-    conexion=crear_conexion()
-    cursor=conexion.cursor()
+#modificar el ranking
+def modificar_ranking(lista:List[Ranking]):
+    conexion=crear_conexion() 
+    cursor=conexion.cursor() #ubicate en la bd despues de conectarte
+    cursor.execute("DELETE FROM ranking_fifa")
+    cursor.executemany(
+        "INSERT INTO ranking_fifa(rango, pais, puntos) VALUES (?,?,?)",
+        [(elementoDeLaLista.rango, elementoDeLaLista.pais, elementoDeLaLista.puntos)for elementoDeLaLista in lista]
+    )
+    conexion.commit() #Ejecuta la consulta en la bd
+    conexion.close()    
+
+#leer el ranking
+def leer_ranking():
+    conexion=crear_conexion() 
+    cursor=conexion.cursor() #ubicate en la bd despues de conectarte
     cursor.execute("SELECT rango, pais, puntos FROM ranking_fifa ORDER BY rango ASC")
     filas=cursor.fetchall()
     conexion.close()
-    return [{"rango":rango,"pais":pais,"puntos":puntos} for (rango, pais, puntos) in filas]
+    return [ {"rango":rango,"pais":pais,"puntos":puntos} for (rango, pais, puntos) in filas]
+
 
 def autocargar_json_si_existe():  # Carga JSON a SQLite si el archivo existe
     if not os.path.exists(FUENTE_DATOS):  # Si no existe JSON, no hace nada
@@ -85,7 +89,7 @@ async def lifespan(app: FastAPI):  # Se ejecuta al iniciar y al cerrar la app
     crear_tabla()  #
     autocargar_json_si_existe()  # Startup: intenta precargar datos del JSON
     yield  # La app queda corriendo entre este yield y el cierre
-    
+      
 
 # Instancia de FastAPI con lifespan moderno
 app = FastAPI(title="API Ranking FIFA con SQLite", lifespan=lifespan)
